d664f3ad2ec8e8765fa304c6f5e839abb6477a86
Cartago <cartago.relaxingcup@gmail.com>
2023-01-15 00:15:48 +0100

    Incorporamos nx al repositorio para cargar mas rapido el cache de lo multiples proyectos

---
+++ projects/02-adminpro/src/app/auth/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styles: [
+    `
+      :host {
+        display: block;
+      }
+    `,
+  ],
+})
+export class LoginComponent {}
