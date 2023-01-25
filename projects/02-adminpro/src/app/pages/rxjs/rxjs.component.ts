4eef88e97302dd41c29dcf90e6b8f9dd20e008ea
Cartago <cartago.relaxingcup@gmail.com>
2023-01-25 20:19:30 +0100

    Terminamos las clases basicas de rxjs

--- projects/02-adminpro/src/app/pages/rxjs/rxjs.component.ts
+++ projects/02-adminpro/src/app/pages/rxjs/rxjs.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
-import { Observable, retry } from 'rxjs';
+import {
+	filter,
+	interval,
+	map,
+	Observable,
+	retry,
+	Subscription,
+	take,
+} from 'rxjs';
 
 @Component({
 	selector: 'app-rxjs',
@@ -7,9 +15,41 @@ import { Observable, retry } from 'rxjs';
 	styles: [],
 })
 export class RxjsComponent {
+	private _subscripcion: Subscription;
+
 	constructor() {
+		// this._retornaObservable().pipe(retry(5)).subscribe({
+		this._subscripcion = this._retornaIntervalo()
+			.pipe()
+			.subscribe({
+				next: (valor) => {
+					console.log('1 Subs:', valor);
+				},
+				error: (error) => {
+					console.warn('Error:', error);
+				},
+				complete: () => {
+					console.log('Se Completo');
+				},
+			});
+	}
+
+	ngOnDestroy(): void {
+		this._subscripcion.unsubscribe();
+	}
+
+	private _retornaIntervalo(): Observable<number> {
+		return interval(100).pipe(
+			// take(4),
+			map((value) => value + 1),
+			filter((valor) => valor % 2 === 0)
+		);
+		// return intervalo$;
+	}
+
+	private _retornaObservable(): Observable<number> {
 		let i = -1;
-		const obs$ = new Observable((observer) => {
+		const obs$ = new Observable<number>((observer) => {
 			const intervalo = setInterval(() => {
 				i++;
 				observer.next(i);
@@ -20,19 +60,6 @@ export class RxjsComponent {
 				if (i === 2) observer.error('Ha llegado a 2');
 			}, 1000);
 		});
-
-		obs$.pipe(
-			retry(5)
-		).subscribe({
-			next: (valor) => {
-				console.log('1 Subs:', valor);
-			},
-			error: (error) => {
-				console.warn('Error:', error);
-			},
-			complete: () => {
-				console.log('Se Completo');
-			},
-		});
+		return obs$;
 	}
 }
