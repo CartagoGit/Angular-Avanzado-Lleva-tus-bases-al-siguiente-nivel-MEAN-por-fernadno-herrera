1fb0f125166a95657c940ed017153ea5acd73fff
Cartago <cartago.relaxingcup@gmail.com>
2023-01-29 16:58:50 +0100

    Añadimos mensaje de error predeterminado controlado

--- projects/03-backend/src/app/controllers/core.controller.ts
+++ projects/03-backend/src/app/controllers/core.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import { getModelSection } from '../helpers/get-model-section.helper';
+import { logError } from '../helpers/logs.helper';
+import { defaultErrorResponse } from '../helpers/auto-responses.helper';
 
 export const coreController = {
 	get: (_req: Request, res: Response) => {
@@ -15,16 +17,18 @@ export const coreController = {
 		});
 	},
 	post: async (req: Request, res: Response) => {
-		const { name, password, email } = req.body;
+		try {
+			const model = getModelSection(req);
+			await model.save();
+			console.log(model);
 
-		const model = getModelSection(req);
-		await model.save();
-		console.log(model);
-
-		res.json({
-			message: 'post',
-			ok: true,
-			data: model,
-		});
+			res.json({
+				message: 'post',
+				ok: true,
+				data: model,
+			});
+		} catch (error) {
+			defaultErrorResponse(res, error as Error, 'MONGO');
+		}
 	},
 };
