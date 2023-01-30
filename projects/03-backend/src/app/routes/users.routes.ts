f954099f66de830da469c006797806278587b8e6
Cartago <cartago.relaxingcup@gmail.com>
2023-01-30 22:31:59 +0100

    Creamos una encriptacion para la contraseña de los usuarios

--- projects/03-backend/src/app/routes/users.routes.ts
+++ projects/03-backend/src/app/routes/users.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '../models/routes.model';
 import { coreRoutes } from './core.routes';
 import { usersMiddlewares } from '../middlewares/users/users.middleware';
+import { Request, Response } from 'express';
+import { CallbackMethod } from '../interfaces/response.interface';
+import bcrypt from 'bcryptjs';
 
 /**
  * * /api/users
@@ -10,6 +13,15 @@ export const usersRoutes: Routes = new Routes({
 	...coreRoutes.routes,
 	post: {
 		...coreRoutes.routes['post'],
+		callback: (req: Request, res: Response) => {
+			//* Encriptamos la contraseña
+			const { password } = req.body;
+			const salt = bcrypt.genSaltSync();
+			req.body.password = bcrypt.hashSync(password, salt);
+
+			//* Se llama a los metodos base de core para realizar el post
+			(coreRoutes.routes['post'].callback as CallbackMethod)(req, res);
+		},
 		middlewares: usersMiddlewares.post,
 	},
 });
