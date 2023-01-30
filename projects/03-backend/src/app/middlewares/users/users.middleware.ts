7a73256ea8fba24a1c94567ac81c0663e65afbd5
Cartago <cartago.relaxingcup@gmail.com>
2023-01-30 22:19:34 +0100

    Refactorizamos el middleware de usuarios

---
+++ projects/03-backend/src/app/middlewares/users/users.middleware.ts
@@ -0,0 +1,37 @@
+import { check } from 'express-validator';
+import {
+	getMessageErrorValidation,
+	getErrorUniqueParam,
+} from '../../helpers/default-responses';
+import { UserModel } from '../../models/mongo/user.model';
+
+export const usersMiddlewares = {
+	post: [
+		check('name', getMessageErrorValidation('name', { required: true }))
+			.not()
+			.isEmpty(),
+		check(
+			'password',
+			getMessageErrorValidation('password', { required: true })
+		)
+			.not()
+			.isEmpty(),
+		check(
+			'email',
+			getMessageErrorValidation('email', {
+				required: true,
+				unique: true,
+				formated: true,
+			})
+		)
+			.isEmail()
+			.not()
+			.isEmpty()
+			.custom(async (email) => {
+				const existEmail = await UserModel.findOne({ email });
+				return (
+					!!existEmail && Promise.reject(getErrorUniqueParam({ email }))
+				);
+			}),
+	],
+};
--- projects/03-backend/src/app/routes/users.routes.ts
+++ projects/03-backend/src/app/routes/users.routes.ts
@@ -1,11 +1,6 @@
 import { Routes } from '../models/routes.model';
 import { coreRoutes } from './core.routes';
-import { UserModel } from '../models/mongo/user.model';
-import { check } from 'express-validator';
-import {
-	getErrorUniqueParam,
-	getMessageErrorValidation,
-} from '../helpers/default-responses';
+import { usersMiddlewares } from '../middlewares/users/users.middleware';
 
 /**
  * * /api/users
@@ -15,33 +10,6 @@ export const usersRoutes: Routes = new Routes({
 	...coreRoutes.routes,
 	post: {
 		...coreRoutes.routes['post'],
-		middlewares: [
-			check('name', getMessageErrorValidation('name', { required: true }))
-				.not()
-				.isEmpty(),
-			check(
-				'password',
-				getMessageErrorValidation('password', { required: true })
-			)
-				.not()
-				.isEmpty(),
-			check(
-				'email',
-				getMessageErrorValidation('email', {
-					required: true,
-					unique: true,
-					formated: true,
-				})
-			)
-				.isEmail()
-				.not()
-				.isEmpty()
-				.custom(async (email) => {
-					const existEmail = await UserModel.findOne({ email });
-					return (
-						!!existEmail && Promise.reject(getErrorUniqueParam({ email }))
-					);
-				}),
-		],
+		middlewares: usersMiddlewares.post,
 	},
 });
