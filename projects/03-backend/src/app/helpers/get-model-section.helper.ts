7071e9cf617747fe5e3531ba80747dae364c6924
Cartago <cartago.relaxingcup@gmail.com>
2023-02-04 13:32:08 +0100

    Corregimos error con los controladores y se añaden a la subscripcion controlandose las respuestas segun la url base de la request

--- projects/03-backend/src/app/helpers/get-model-section.helper.ts
+++ projects/03-backend/src/app/helpers/get-model-section.helper.ts
@@ -13,6 +13,7 @@ export const getModelSection = (req: Request): Model<unknown> => {
 
 export const getSectionFromUrl = (req: Request) => {
 //TODO CORREGIR
-	const baseUrlSections = req.originalUrl.split('/');
-	return baseUrlSections[baseUrlSections.length - 2];
+console.log(req.baseUrl, req.originalUrl);
+	const baseUrlSections = req.baseUrl.split('/');
+	return baseUrlSections[baseUrlSections.length - 1];
 };
