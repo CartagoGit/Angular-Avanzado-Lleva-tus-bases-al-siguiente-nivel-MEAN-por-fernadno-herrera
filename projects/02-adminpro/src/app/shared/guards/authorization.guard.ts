c8974357025b426439331592fdb920866c34c1c1
Cartago <cartago.relaxingcup@gmail.com>
2023-03-13 21:35:01 +0100

    Limpiamos los guards de auth y dashboard ante al falla de rutas, y comenzamos de nuevo los guards

--- projects/02-adminpro/src/app/shared/guards/authorization.guard.ts
+++ projects/02-adminpro/src/app/shared/guards/authorization.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable, of, catchError, map } from 'rxjs';
+import { Observable } from 'rxjs';
 import { paths } from '../constants/paths.constant';
 import { AuthService } from '../services/http/auth.service';
 import { StateService } from '../services/settings/state.service';
@@ -16,7 +16,6 @@ export class AuthorizationGuard {
 	// ANCHOR : constructor
 	constructor(
 		private _router: Router,
-		private _authSvc: AuthService,
 		private _stateSvc: StateService
 	) {}
 
@@ -26,13 +25,6 @@ export class AuthorizationGuard {
 			this._router.navigate([this._maintenancePath?.fullPath]);
 			return false;
 		}
-		return this._authSvc.renewToken().pipe(
-			map((resp) => {
-				if (!resp) throw 'No hay token';
-				this._router.navigate([this._dashboardPath?.fullPath]);
-				return false;
-			}),
-			catchError(() => of(true))
-		);
+		return true;
 	}
 }
--- projects/02-adminpro/src/app/shared/guards/dashboard.guard.ts
+++ projects/02-adminpro/src/app/shared/guards/dashboard.guard.ts
@@ -1,27 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { catchError, map, Observable, of } from 'rxjs';
 import { paths } from '../constants/paths.constant';
-import { AuthService } from '../services/http/auth.service';
+import { StateService } from '../services/settings/state.service';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class DashboardGuard {
 	// ANCHOR : variables
+	private _maintenancePath = paths.getPath('maintenance');
 	private _loginPath = paths.getPath('login');
 
 	// ANCHOR : constructor
-	constructor(private _router: Router, private _authSvc: AuthService) {}
+	constructor(private _router: Router, private _stateSvc: StateService) {}
 
 	// ANCHOR : methods
-	canMatch(): Observable<boolean> {
-		return this._authSvc.renewToken().pipe(
-			map(() => true),
-			catchError(() => {
-				this._router.navigate([this._loginPath?.fullPath]);
-				return of(false);
-			})
-		);
+	canMatch(): boolean {
+		if (this._stateSvc.isMaintenance) {
+			this._router.navigate([this._maintenancePath?.fullPath]);
+			return false;
+		}
+		return true;
 	}
 }
