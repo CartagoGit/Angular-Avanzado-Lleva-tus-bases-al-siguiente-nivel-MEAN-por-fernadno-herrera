1e1585d20a3245cbb3b2f203b62056f1f157d75b
Cartago <cartago.relaxingcup@gmail.com>
2023-03-21 22:41:34 +0100

    deshabilitamos los formularios para modificar el perfil si el usuario es de google

--- projects/02-adminpro/src/app/pages/general/general.module.ts
+++ projects/02-adminpro/src/app/pages/general/general.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
 import { ProfileComponent } from './profile/profile.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
 	declarations: [AccountSettingsComponent, ProfileComponent],
-	imports: [CommonModule],
+	imports: [CommonModule, ReactiveFormsModule],
 	exports: [AccountSettingsComponent, ProfileComponent],
 })
 export class GeneralModule {}
--- projects/02-adminpro/src/app/pages/general/profile/profile.component.ts
+++ projects/02-adminpro/src/app/pages/general/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { User } from '../../../shared/models/mongo-models/user.model';
+import { StateService } from '../../../shared/services/settings/state.service';
 
 @Component({
 	selector: 'app-profile',
@@ -6,5 +9,33 @@ import { Component } from '@angular/core';
 	styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent {
-	public showPassword : boolean = false;
+	// ANCHOR : Variables
+	public showPassword: boolean = false;
+	public isSubmited: boolean = false;
+
+	public user: User;
+
+	public profileForm!: FormGroup;
+
+	// ANCHOR : Constructor
+	constructor(private _fb: FormBuilder, private _state: StateService) {
+		this.user = this._state.user!;
+	}
+
+	ngOnInit(): void {
+		this.profileForm = this._fb.group({
+			name: [
+				{ value: this.user.name, disabled: this.user.google },
+				[Validators.required],
+			],
+			email: [
+				{ value: this.user.email, disabled: this.user.google },
+				[Validators.required, Validators.email],
+			],
+			password: [{ value: '', disabled: this.user.google }],
+			password2: [{ value: '', disabled: this.user.google }],
+		});
+	}
+
+	// ANCHOR : Methods
 }
