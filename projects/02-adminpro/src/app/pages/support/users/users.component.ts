f3a2703b4dc81c0c09f3c6a1b528673e71ed6162
Cartago <cartago.relaxingcup@gmail.com>
2023-04-02 12:57:13 +0200

    Creamos footer y total y añadimos sus estilos

--- projects/02-adminpro/src/app/pages/support/users/users.component.ts
+++ projects/02-adminpro/src/app/pages/support/users/users.component.ts
@@ -5,4 +5,19 @@ import { Component } from '@angular/core';
 	templateUrl: './users.component.html',
 	styleUrls: ['./users.component.css'],
 })
-export class UsersComponent {}
+export class UsersComponent {
+	// ANCHOR - Variables
+	public users: any[] = [];
+
+	// ANCHOR - Constructor
+	constructor() {
+		this.users = [
+			{
+				name: 'John Doe',
+				email: '	ada',
+			},
+		];
+	}
+
+	// ANCHOR - Methods
+}
