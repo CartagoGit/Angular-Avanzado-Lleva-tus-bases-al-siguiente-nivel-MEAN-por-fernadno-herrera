ee5f67beaf0598615f7ef1742c5298987067bea7
Cartago <cartago.relaxingcup@gmail.com>
2023-04-17 20:43:23 +0200

    Creamos el modal que se usara para ver y añadir docotres a un hospital

---
+++ projects/02-adminpro/src/app/modals/doctos-of-hospital-modal/doctos-of-hospital-modal.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-doctos-of-hospital-modal',
+	templateUrl: './doctos-of-hospital-modal.component.html',
+	styleUrls: ['./doctos-of-hospital-modal.component.css'],
+})
+export class DoctosOfHospitalModalComponent {}
--- projects/02-adminpro/src/app/modals/modals.module.ts
+++ projects/02-adminpro/src/app/modals/modals.module.ts
@@ -5,10 +5,19 @@ import { ImageModalComponent } from './image-modal/image-modal.component';
 import { HospitalModalComponent } from './hospital-modal/hospital-modal.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
+import { DoctosOfHospitalModalComponent } from './doctos-of-hospital-modal/doctos-of-hospital-modal.component';
 
 @NgModule({
-	declarations: [ImageModalComponent, HospitalModalComponent],
+	declarations: [
+		ImageModalComponent,
+		HospitalModalComponent,
+		DoctosOfHospitalModalComponent,
+	],
 	imports: [CommonModule, ReactiveFormsModule, SharedModule],
-	exports: [ImageModalComponent, HospitalModalComponent],
+	exports: [
+		ImageModalComponent,
+		HospitalModalComponent,
+		DoctosOfHospitalModalComponent,
+	],
 })
 export class ModalsModule {}
