3e3c709f2c9f87ac4c0a2f62cbe07578e17a1096
Cartago <cartago.relaxingcup@gmail.com>
2023-04-17 21:22:08 +0200

    Limitamos la entrada al modal de doctores si no se envia los datos del hospital

--- projects/02-adminpro/src/app/modals/doctos-of-hospital-modal/doctors-of-hospital-modal.component.ts
+++ projects/02-adminpro/src/app/modals/doctos-of-hospital-modal/doctors-of-hospital-modal.component.ts
@@ -1,8 +1,40 @@
 import { Component } from '@angular/core';
+import { ModalService } from '../../shared/services/settings/modal.service';
+import { Hospital } from '../../shared/models/mongo-models/hospital.model';
+import { SweetAlertService } from '../../shared/services/helpers/sweet-alert.service';
+import { isHospital } from '../../shared/helpers/models.helpers';
 
 @Component({
 	selector: 'app-doctors-of-hospital-modal',
 	templateUrl: './doctors-of-hospital-modal.component.html',
 	styleUrls: ['./doctors-of-hospital-modal.component.css'],
 })
-export class DoctorsOfHospitalModalComponent {}
+export class DoctorsOfHospitalModalComponent {
+	// ANCHOR : Variables
+	public data!: Hospital;
+
+	// ANCHOR : Constructor
+	constructor(
+		private _modalSvc: ModalService,
+		private _sweerAlertSvc: SweetAlertService
+	) {}
+
+	ngOnInit(): void {
+		if (!this.data || !isHospital(this.data)) {
+			this._sweerAlertSvc.alertError(
+				'This modal need a data hospital to work'
+			);
+			this.close();
+		}
+	}
+
+	// ANCHOR : Methods
+
+	/**
+	 * ? Cierra el modal
+	 * @public
+	 */
+	public close() {
+		this._modalSvc.close();
+	}
+}
