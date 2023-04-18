1d12d5b1eeac43a8d32e49c1824031275aebd91e
Cartago <cartago.relaxingcup@gmail.com>
2023-04-18 21:28:49 +0200

    Creamos dos arrays de pruebas para afinar los dragables

--- projects/02-adminpro/src/app/modals/doctos-of-hospital-modal/doctors-of-hospital-modal.component.ts
+++ projects/02-adminpro/src/app/modals/doctos-of-hospital-modal/doctors-of-hospital-modal.component.ts
@@ -22,6 +22,34 @@ export class DoctorsOfHospitalModalComponent {
 
 	public areDoctorsChanged: boolean = false;
 
+	public array1 = [
+		{ id: 1, name: 'John' },
+		{ id: 2, name: 'Doe' },
+		{ id: 3, name: 'Jane' },
+		{ id: 4, name: 'Pepe' },
+		{ id: 5, name: 'Juan' },
+		{ id: 6, name: 'Pablo' },
+		{ id: 7, name: 'Pedro' },
+		{ id: 8, name: 'Pepito' },
+		{ id: 9, name: 'Pepa' },
+		{ id: 10, name: 'Pepita' },
+		{ id: 11, name: 'Pepin' },
+		{ id: 12, name: 'Pepinillo' },
+		{ id: 13, name: 'Anibal' },
+		{ id: 14, name: 'Anibalito' },
+		{ id: 15, name: 'Anibalina' },
+	];
+
+	public array2 = [
+		{ id: 1, name: 'Urraca' },
+		{ id: 2, name: 'Urracita' },
+		{ id: 3, name: 'Urracito' },
+		{ id: 4, name: 'Urracina' },
+		{ id: 5, name: 'Teruel' },
+		{ id: 6, name: 'Teruelita' },
+		{ id: 7, name: 'Teruelito' },
+	];
+
 	// ANCHOR : Constructor
 	constructor(
 		private _modalSvc: ModalService,
@@ -61,7 +89,10 @@ export class DoctorsOfHospitalModalComponent {
 				this.doctorsOfHospital = doctors.map(
 					(doctor) => new Doctor(doctor)
 				);
-				console.log("❗this._hospitalsSvc.getDoctors  ➽ doctorsOfHospital ➽ ⏩" , this.doctorsOfHospital);
+				console.log(
+					'❗this._hospitalsSvc.getDoctors  ➽ doctorsOfHospital ➽ ⏩',
+					this.doctorsOfHospital
+				);
 			},
 			error: (error: DefaultErrorResponse) => {
 				this._sweerAlertSvc.alertError(error.error_message);
@@ -88,7 +119,10 @@ export class DoctorsOfHospitalModalComponent {
 				this.doctorsWithoutHospital = doctors.map(
 					(doctor) => new Doctor(doctor)
 				);
-				console.log("❗this._doctorsSvc.getByQuery  ➽ doctorsWithoutHospital ➽ ⏩" , this.doctorsWithoutHospital);
+				console.log(
+					'❗this._doctorsSvc.getByQuery  ➽ doctorsWithoutHospital ➽ ⏩',
+					this.doctorsWithoutHospital
+				);
 			},
 			error: (error: DefaultErrorResponse) => {
 				this._sweerAlertSvc.alertError(error.error_message);
@@ -110,23 +144,39 @@ export class DoctorsOfHospitalModalComponent {
 		console.log(this.doctorsOfHospital);
 	}
 
-
+	/**
+	 * ? Elemento que se esta arrastrando
+	 * @public
+	 * @param {DragEvent} event
+	 */
 	public onDragStart(event: DragEvent): void {
-		console.log("❗onDragStart  ➽ event ➽ ⏩" , event);
-		event.dataTransfer?.setData('text/plain', (event.target as HTMLElement)?.textContent!);
-	 }
+		console.log('❗onDragStart  ➽ event ➽ ⏩', event);
+		event.dataTransfer?.setData(
+			'text/plain',
+			(event.target as HTMLElement)?.textContent!
+		);
+	}
 
-	 public onDragOver(event: DragEvent): void {
-		console.log("❗onDragOver  ➽ event ➽ ⏩" , event);
+	/**
+	 * ? Evita el evento por defecto cuando se arrastra un elemento sobre otro
+	 * @public
+	 * @param {DragEvent} event
+	 */
+	public onDragOver(event: DragEvent): void {
 		event.preventDefault();
-	 }
+	}
 
-	 public onDrop(event: DragEvent): void {
-		console.log("❗onDrop  ➽ event ➽ ⏩" , event);
-		if(!event || !event.target) return;
+	/**
+	 * ? Cuando se suelta el elemento arrastrado
+	 * @public
+	 * @param {DragEvent} event
+	 */
+	public onDrop(event: DragEvent): void {
+		console.log('❗onDrop  ➽ event ➽ ⏩', event);
+		if (!event || !event.target) return;
 		const text = event.dataTransfer?.getData('text/plain');
 		const li = document.createElement('li');
 		li.textContent = text || 'no existe';
 		(event.target as HTMLElement).appendChild(li);
-	 }
+	}
 }
