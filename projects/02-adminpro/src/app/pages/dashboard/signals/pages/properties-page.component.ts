368bfd7ed05aad373cb55312ee02f045eaee6125
Cartago <cartago.relaxingcup@gmail.com>
2023-05-18 20:26:06 +0200

    Creamos la actualizacion con signals

--- projects/02-adminpro/src/app/pages/dashboard/signals/pages/properties-page.component.ts
+++ projects/02-adminpro/src/app/pages/dashboard/signals/pages/properties-page.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, WritableSignal, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { User } from '../interfaces/user-request.interface';
 
 @Component({
 	selector: 'app-properties-page',
@@ -11,28 +12,41 @@ import { CommonModule } from '@angular/common';
 		<div class="card-body d-flex flex-column align-items-center">
 			<div class="row">
 				<div class="col-6 d-flex flex-column g-15">
-					<input
-						#txtEmail
-						type="text"
-						class="form-control"
-						(input)="onFieldUpdated('email', txtEmail.value)"
-					/>
-					<input
-						#txtFirstName
-						type="text"
-						class="form-control"
-						(input)="onFieldUpdated('firstName', txtFirstName.value)"
-					/>
-					<input
-						#txtLastName
-						type="text"
-						class="form-control"
-						(input)="onFieldUpdated('lastName', txtLastName.value)"
-					/>
+					<div class="input-info d-flex flex-column g-10">
+						<span>Email: </span>
+						<input
+							#txtEmail
+							type="text"
+							class="form-control"
+							(input)="onFieldUpdated('email', txtEmail.value)"
+							[value]="user().email"
+						/>
+					</div>
+					<div class="input-info d-flex flex-column g-10">
+						<span>First Name</span>
+						<input
+							#txtFirstName
+							type="text"
+							class="form-control"
+							(input)="onFieldUpdated('first_name', txtFirstName.value)"
+							[value]="user().first_name"
+						/>
+					</div>
+					<div class="input-info d-flex flex-column g-10">
+						<span>Last Name</span>
+						<input
+							#txtLastName
+							type="text"
+							class="form-control"
+							(input)="onFieldUpdated('last_name', txtLastName.value)"
+							[value]="user().last_name"
+						/>
+					</div>
 				</div>
 
 				<div class="col-6">
-					<pre>{{ 0 }}</pre>
+					<pre>{{ user() | json }}</pre>
+					<span>Full name: {{ fullName() }}</span>
 				</div>
 			</div>
 		</div>`,
@@ -40,15 +54,24 @@ import { CommonModule } from '@angular/common';
 })
 export class PropertiesPageComponent {
 	// ANCHOR : Variables
+	public user: WritableSignal<User> = signal({
+		avatar: 'https://reqres.in/img/faces/1-image.jpg',
+		email: 'george.bluth@reqres.in',
+		first_name: 'George',
+		id: 1,
+		last_name: 'Bluth',
+	});
+
+	public fullName = computed(() => {
+		return `${this.user().first_name} ${this.user().last_name}`;
+	});
 
 	// ANCHOR : Constructor
 	constructor() {}
 
 	// ANCHOR : Methods
-	public onFieldUpdated(
-		field: 'email' | 'firstName' | 'lastName',
-		value: string
-	): void {
+	public onFieldUpdated(field: keyof User, value: string): void {
 		console.log(field, value);
+		this.user.update((user) => ({ ...user, [field]: value }));
 	}
 }
