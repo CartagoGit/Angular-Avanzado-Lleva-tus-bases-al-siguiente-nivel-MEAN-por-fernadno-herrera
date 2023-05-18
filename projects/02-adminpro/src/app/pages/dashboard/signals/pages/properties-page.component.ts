d6c7109f3402657b25cb92301e9da9dd99799d49
Cartago <cartago.relaxingcup@gmail.com>
2023-05-18 20:09:12 +0200

    Comenzamos el ejercicio de mutaciones con signals

--- projects/02-adminpro/src/app/pages/dashboard/signals/pages/properties-page.component.ts
+++ projects/02-adminpro/src/app/pages/dashboard/signals/pages/properties-page.component.ts
@@ -5,7 +5,35 @@ import { CommonModule } from '@angular/common';
 	selector: 'app-properties-page',
 	standalone: true,
 	imports: [CommonModule],
-	template: `<p>properties-page works!</p>`,
+	template: ` <div class="card-header d-flex justify-content-center">
+			<h2>Properties and mutations with signals</h2>
+		</div>
+		<div class="card-body d-flex flex-column align-items-center">
+			<div class="row">
+				<div class="col-6">
+					<input
+						#txtEmail
+						type="text"
+						class="form-control"
+						(input)="onFieldUpdated('email', txtEmail.value)"
+					/>
+				</div>
+
+				<div class="col-6">
+					<h3>Mutations</h3>
+				</div>
+			</div>
+		</div>`,
 	styles: [],
 })
-export class PropertiesPageComponent {}
+export class PropertiesPageComponent {
+	// ANCHOR : Variables
+
+	// ANCHOR : Constructor
+	constructor() {}
+
+	// ANCHOR : Methods
+	public onFieldUpdated(field: string, value: string): void {
+		console.log(field, value);
+	}
+}
