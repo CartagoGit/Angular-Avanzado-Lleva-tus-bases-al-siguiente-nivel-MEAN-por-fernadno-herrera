199583ce35ad3263d74d458bd1872c9afa51515b
Cartago <cartago.relaxingcup@gmail.com>
2023-05-28 22:35:07 +0200

    Desestructuramos los datos y añadimos excepcion si la data no se recibe correctamente

--- projects/02-adminpro/src/app/pages/general/global-search/global-search.component.ts
+++ projects/02-adminpro/src/app/pages/general/global-search/global-search.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, WritableSignal, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EverywhereService } from '../../../shared/services/http/everywhere.service';
+import { DefaultErrorResponse } from '../../../shared/interfaces/http/response.interfaces';
+import { SweetAlertService } from '../../../shared/services/helpers/sweet-alert.service';
+import { tap } from 'rxjs';
+import { CollectionModels } from '../../../shared/interfaces/http/everywhere.interfaces';
 
 @Component({
 	selector: 'app-global-search',
@@ -10,10 +14,14 @@ import { EverywhereService } from '../../../shared/services/http/everywhere.serv
 export class GlobalSearchComponent {
 	// ANCHOR : Variables
 
+	public results: WritableSignal<CollectionModels | undefined> =
+		signal(undefined);
+
 	// ANCHOR : Constructor
 	constructor(
 		private _activatedRoute: ActivatedRoute,
-		private _everywhereSvc: EverywhereService
+		private _everywhereSvc: EverywhereService,
+		private _sweetAlertSvc: SweetAlertService
 	) {}
 
 	ngOnInit(): void {
@@ -32,10 +40,32 @@ export class GlobalSearchComponent {
 	 */
 	private _search(query: string): void {
 		console.log(query);
-		this._everywhereSvc.getFrom({ field: 'name', search: query }, {}).subscribe(
-			(data) => {
-				console.log(data);
-			}
-		);
+		this._everywhereSvc
+			.getFrom(
+				{ field: 'name', search: query },
+				{ limit: 10, pagination: true }
+			)
+			.pipe(
+				tap((resp) => {
+					const { ok } = resp;
+					if (ok) return;
+					throw resp;
+				})
+			)
+			.subscribe({
+				next: (resp) => {
+					const {
+						data: { Users, Doctors, Hospitals },
+					} = resp;
+
+					console.log('Users', Users);
+					console.log('Doctors', Doctors);
+					console.log('Hospitals', Hospitals);
+				},
+				error: (error: DefaultErrorResponse) => {
+					console.error(error);
+					this._sweetAlertSvc.alertError(error.error_message);
+				},
+			});
 	}
 }
