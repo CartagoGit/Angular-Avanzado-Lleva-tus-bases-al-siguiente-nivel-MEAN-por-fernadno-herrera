cff627f1401113203d14879560e3b933986d34da
Cartago <cartago.relaxingcup@gmail.com>
2023-05-28 13:01:42 +0200

    Creamos componente para busquedas globales

--- projects/02-adminpro/src/app/pages/general/general.module.ts
+++ projects/02-adminpro/src/app/pages/general/general.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
 import { ProfileComponent } from './profile/profile.component';
+import { GlobalSearchComponent } from './global-search/global-search.component';
+
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../../shared/shared.module';
 import { GeneralRoutingModule } from './general.routing';
 
 @NgModule({
-	declarations: [AccountSettingsComponent, ProfileComponent],
+	declarations: [
+		AccountSettingsComponent,
+		ProfileComponent,
+		GlobalSearchComponent,
+	],
 	imports: [
 		CommonModule,
 		GeneralRoutingModule,
