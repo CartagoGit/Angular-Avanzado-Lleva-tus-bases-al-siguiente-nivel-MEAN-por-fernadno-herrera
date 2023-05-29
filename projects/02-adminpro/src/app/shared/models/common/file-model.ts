1e6bce182bbbd38ac90f4e7fe6d0bd6c9436fc61
Cartago <cartago.relaxingcup@gmail.com>
2023-05-29 17:19:24 +0200

    Cambiamos el modelo de FileModel por uno que devuelve el url y la base64 y blob

--- projects/02-adminpro/src/app/shared/models/common/file-model.ts
+++ projects/02-adminpro/src/app/shared/models/common/file-model.ts
@@ -9,26 +9,73 @@ export interface FileModelProps {
 	readonly name: string;
 	readonly file: File | undefined;
 	readonly url: string | undefined;
-}
+ }
 
-/**
- * ? Modelo para manejar las imagenes
- * @export
- * @class ImageFiles
- * @typedef {ImageFiles}
- */
-export class FileModel implements FileModelProps {
+ /**
+  * ? Interface de archivo con blob y base 64
+  * @export
+  * @interface FileWithUrls
+  * @typedef {FileWithUrls}
+  */
+ export interface FileWithUrls {
+	file: File;
+	url: string;
+	urlBase64: string;
+	urlBase64WithoutStart: string;
+ }
+
+ /**
+  * ? Modelo para manejar las imagenes
+  * @export
+  * @class ImageFiles
+  * @typedef {ImageFiles}
+  */
+ export class FileModel implements FileModelProps {
 	// ANCHOR : Variables
 	public filesArray: File[] = [];
 	public readonly name: string = '';
 	public readonly file: File | undefined;
 	public readonly url: string | undefined;
+	public fileWithUrls: FileWithUrls[] = [];
 
 	// ANCHOR : Constructor
 	constructor(data?: { filesArray: File[] }) {
-		this.filesArray = data?.filesArray || [];
-		this.name = this.filesArray.length > 0 ? this.filesArray[0].name : '';
-		this.file = this.filesArray.length > 0 ? this.filesArray[0] : undefined;
-		this.url = this.file ? URL.createObjectURL(this.file) : undefined;
+	  this.filesArray = data?.filesArray || [];
+
+	  Promise.all(
+		 this.filesArray.map(async (file) => {
+			const fileWithUrl = await this._readFile(file);
+			return fileWithUrl;
+		 })
+	  ).then((files) => (this.fileWithUrls = files));
+
+	  this.name = this.filesArray.length > 0 ? this.filesArray[0].name : '';
+	  this.file = this.filesArray.length > 0 ? this.filesArray[0] : undefined;
+	  this.url = this.file ? URL.createObjectURL(this.file) : undefined;
 	}
-}
+
+
+	/**
+	 * ? Convierte un archivo blob a base64
+	 * @private
+	 * @param {File} file
+	 * @returns {Promise<FileWithUrls>}
+	 */
+	private _readFile(file: File): Promise<FileWithUrls> {
+	  return new Promise((resolve, reject) => {
+		 const reader = new FileReader();
+		 reader.readAsDataURL(file);
+		 reader.onload = () => {
+			const urlBase64 = reader.result as string;
+			const urlBase64WithoutStart = urlBase64.split('base64,')[1];
+			resolve({
+			  file,
+			  url: URL.createObjectURL(file),
+			  urlBase64,
+			  urlBase64WithoutStart,
+			});
+		 };
+		 reader.onerror = () => reject(reader.error as any);
+	  });
+	}
+ }
