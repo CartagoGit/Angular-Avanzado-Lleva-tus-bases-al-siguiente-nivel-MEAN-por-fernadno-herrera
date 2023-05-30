4874b539b120da33c4fa98969e0fab6beb39a72d
Cartago <cartago.relaxingcup@gmail.com>
2023-05-31 01:16:45 +0200

    Probamos con el url hardcodeado de la api

--- projects/02-adminpro/src/environments/environment.prod.ts
+++ projects/02-adminpro/src/environments/environment.prod.ts
@@ -1,14 +1,17 @@
 import { Config } from './config.model';
 
-import * as dotenv from 'dotenv';
-dotenv.config();
+// import * as dotenv from 'dotenv';
+// dotenv.config();
 
 export const environment: Config = new Config({
 	API_PORT: process.env['PORT'] || undefined,
 	MODE: 'production',
-	API_URL_BASE: process.env['API_URL_BASE'] || '',
-	GOOGLE_ID: process.env['GOOGLE_ID'] || '',
-	MAPBOX_KEY: process.env['MAPBOX_KEY'] || '',
+	API_URL_BASE: 'https://backend-angular-avanzado.onrender.com',
+	// process.env['API_URL_BASE'] || '',
+	GOOGLE_ID: '',
+	// process.env['GOOGLE_ID'] || '',
+	MAPBOX_KEY: '',
+	// process.env['MAPBOX_KEY'] || '',
 });
 
 export const config = environment;
