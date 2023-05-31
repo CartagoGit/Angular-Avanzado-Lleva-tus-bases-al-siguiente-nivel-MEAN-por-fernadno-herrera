8d2709fbc51900eef6f3470111c9daf71efc7f78
Cartago <cartago.relaxingcup@gmail.com>
2023-05-31 19:24:57 +0200

    Añadimos mensaje en el front en el caso de que el backend no este disponible

--- projects/02-adminpro/src/app/shared/interceptors/change-type-response.interceptor.ts
+++ projects/02-adminpro/src/app/shared/interceptors/change-type-response.interceptor.ts
@@ -8,13 +8,13 @@ import {
 } from '@angular/common/http';
 import { Observable, catchError, throwError, retry } from 'rxjs';
 import { DefaultErrorResponse } from '../interfaces/http/response.interfaces';
-
+import { SweetAlertService } from '../services/helpers/sweet-alert.service';
 
 @Injectable()
 export class ChangeTypeResponseInterceptor implements HttpInterceptor {
 	private _retryDelay = 100;
 	private _retryMaxAttempts = 1;
-	constructor() {}
+	constructor(private _sweetAlertSvc: SweetAlertService) {}
 
 	/**
 	 * ? Interceptor que cambia la response en caso de error en las respuestas Http, y reintenta la peticion en caso de error
@@ -30,6 +30,9 @@ export class ChangeTypeResponseInterceptor implements HttpInterceptor {
 		return next.handle(request).pipe(
 			// retry({ count: this._retryMaxAttempts, delay: this._retryDelay }),
 			catchError((httpErrorResponse: HttpErrorResponse) => {
+				if (httpErrorResponse.status === 503)
+					this._sweetAlertSvc.alertError('BACKEND IS NOT AVAIBLE');
+
 				return throwError(
 					() => httpErrorResponse.error as DefaultErrorResponse
 				);
